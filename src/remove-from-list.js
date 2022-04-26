8fa207d9bb418af2d7068830ce53927fbd37c8fb
Hanna Papova <seal.annuska@gmail.com>
2022-04-26 17:18:32 -0400

    feat: add linked list task solution

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,11 +22,54 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function removeKFromList(l, k) {
+  let head = l; // to return save the head
+
+  // check the head
+  if (head.value === k) {
+    // if there are more nodes set next node as the new head
+    if (head.next) {
+      let temp = head.next;
+      head = null;
+      head = temp;
+    } else {
+      // else set the list as null
+      head = null;
+    }
+  }
+
+  let currNode = head;
+  let prevNode = null;
+  let nextNode = currNode.next;
+
+  // while there are nodes to check
+  while (currNode) {
+    if (currNode.value === k) removeNode(prevNode, currNode, nextNode, k);
+
+    prevNode = currNode;
+    currNode = prevNode.next;
+    nextNode = currNode.next;
+  }
+
+  return head;
+}
+
+function removeNode(prevNode, currNode, nextNode, k) {
+  if (!nextNode) { // tail
+    prevNode.next = null;
+    currNode = null;
+  } else if (nextNode.value === k) { // double k case
+    prevNode.next = nextNode.next;
+    currNode.next = null;
+    currNode = null;
+  } else { // middle node
+    prevNode.next = nextNode;
+    currNode.next = null;
+    currNode = null;
+  }
 }
 
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
