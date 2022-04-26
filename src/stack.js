b262765bd65079df4480822e9330df875ce8fcd0
Hanna Papova <seal.annuska@gmail.com>
2022-04-26 17:50:24 -0400

    feat: add stact task solution

--- src/stack.js
+++ src/stack.js
@@ -12,23 +12,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
+let stack = [];
+
+// LIFO – last in first out principle
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  push(element) {
+    stack.unshift(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (stack.length === 0) return undefined;
+    return stack.shift();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return stack[0];
   }
 }
 
 module.exports = {
-  Stack
+  Stack,
 };
